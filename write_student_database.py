6dedec262f8eab85bc578e26aa45516ebfa9e223
kosmik247 <kosmik247@gmail.com>
2023-01-25 09:35:11 +0000

    New DB Student Project

--- write_student_database.py
+++ write_student_database.py
@@ -2,7 +2,7 @@ import sqlite3 as sql
 
 # This creates an sqlite file if it doesn't exist and opens it otherwise
 
-with sql.connect('student.db') as conn:
+with sql.connect('SchoolDB/student.db') as conn:
 
     # The cursor is used to read and write to the database
 
